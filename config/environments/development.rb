ba7a587ed4f82f84c1c9cc13d0ab74b994a7ff04
Marcelo Martins <marcelo.martins@cilia.com.br>
2017-04-15 00:29:16 -0300

    Creation Of Models Note, Tag, Tags Notes and User Notes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # Mailer
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.default_url_options = {:host => "localhost:3000"}
